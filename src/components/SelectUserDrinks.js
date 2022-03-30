f626fdc5310f31a5e62a355879c3b8c3fed2e090
Vincent Buchholz <vincbuchholz@gmail.com>
2022-03-30 15:17:52 +0200

    select drinks not done

--- src/components/SelectUserDrinks.js
+++ src/components/SelectUserDrinks.js
@@ -2,25 +2,46 @@ import React, {useEffect, useState} from 'react';
 
 const SelectUserDrinks = ({userDrinks, setUserDrinks}) => {
     const[userDrinkOptions,setUserDrinkOptions] = useState();
+
+    var uDrinks = null;
+
+
     
 
-    async function getUserDrinks () {
-        // e.preventDefault()
-        await fetch("http://localhost:5001/drinks")
-            .then(res => res.json())
-            .then(data => {
-                setUserDrinks(data)
-                console.log(userDrinks)
-            })
-            .catch(err => console.log(err))
-    }
-    useEffect(() => {
+    // async function getUserDrinks () {
+    //     // e.preventDefault()
+    //     await fetch("http://localhost:5001/drinks")
+    //         .then(res => res.json())
+    //         .then(data => {
+    //           return data;
+    //         })
+    //         .catch(err => console.log(err))
+    // }
 
+
+    useEffect(() => {
+            fetch("http://localhost:5001/drinks")
+                .then(res => res.json())
+                .then(data => {
+                    setUserDrinks(data);
+                })
+                .catch(err => console.log(err))
     }, []);
+
+    if(userDrinks) {
+        uDrinks = userDrinks.map((drink) => {
+          return `<option>${drink.strDrink}</option>`
+        })
+
+    }
+
+
+
+
 
     return (
         <>
-            <select></select>
+            <select>{uDrinks}</select>
         </>
     );
 };
