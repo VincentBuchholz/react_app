070b3a01fa5e45db1a882a29e7a19e34554a9592
viggo23111 <78900579+viggo23111@users.noreply.github.com>
2022-03-31 10:30:58 +0200

    Adds create drink form

--- src/App.js
+++ src/App.js
@@ -10,49 +10,49 @@ import './styles/drink-list.css'
 
 
 export default function App() {
-  const[drink,setDrink] = useState();
-  const[imgUrl,setImgUrl] = useState();
-  const [instr,setInstr] = useState();
-  const [searchInput, setSearchInput] = useState();
-  const [userDrinks, setUserDrinks] = useState();
+    const [drink, setDrink] = useState();
+    const [imgUrl, setImgUrl] = useState();
+    const [instr, setInstr] = useState();
+    const [searchInput, setSearchInput] = useState();
+    const [userDrinks, setUserDrinks] = useState([{id: "", strDrink: "", strInstructions: "", strDrinkThumb: ""}]);
 
-  useEffect(()=>{
-const getUserDrinks = () => {
-  fetch("http://localhost:5001/drinks")
-      .then(res => res.json())
-      .then(data => {
-        setUserDrinks(data)
-      })
-      .catch(err => console.log(err))
-}
+    useEffect(() => {
+        const getUserDrinks = () => {
+            fetch("http://localhost:5001/drinks")
+                .then(res => res.json())
+                .then(data => {
+                    setUserDrinks(data)
+                })
+                .catch(err => console.log(err))
+        }
 
-getUserDrinks();
+        getUserDrinks();
 
-  },[setUserDrinks])
+    }, [setUserDrinks])
 
 
-  console.log(JSON.stringify(userDrinks))
+    console.log(JSON.stringify(userDrinks))
 
-  return <div className="App">
-  <RandomDrink drink={drink}
-               setDrink={setDrink}
-               imgUrl={imgUrl}
-               setImgUrl={setImgUrl}
-               instr={instr}
-               setInstr={setInstr}
+    return <div className="App">
+        <RandomDrink drink={drink}
+                     setDrink={setDrink}
+                     imgUrl={imgUrl}
+                     setImgUrl={setImgUrl}
+                     instr={instr}
+                     setInstr={setInstr}
 
-  />
-    <SearchDrink searchInput={searchInput}
-                 setSearchInput={setSearchInput}
-                 setDrink={setDrink}
-                 setImgUrl={setImgUrl}
-                 instr={instr}
-                 setInstr={setInstr}/>
+        />
+        <SearchDrink searchInput={searchInput}
+                     setSearchInput={setSearchInput}
+                     setDrink={setDrink}
+                     setImgUrl={setImgUrl}
+                     instr={instr}
+                     setInstr={setInstr}/>
 
-    <DisplayDrink drink={drink}
-    imgUrl={imgUrl}
-    instr={instr}/>
-    <CreateDrink setUserDrinks={setUserDrinks} userDrinks={userDrinks}/>
-    <ListUserDrinks  userDrinks={userDrinks} setUserDrinks={setUserDrinks} />
-  </div>
+        <DisplayDrink drink={drink}
+                      imgUrl={imgUrl}
+                      instr={instr}/>
+        <CreateDrink setUserDrinks={setUserDrinks} userDrinks={userDrinks}/>
+        <ListUserDrinks userDrinks={userDrinks} setUserDrinks={setUserDrinks}/>
+    </div>
 }
--- src/components/CreateDrink.js
+++ src/components/CreateDrink.js
@@ -1,41 +1,51 @@
 import React, {useEffect, useState} from 'react';
 
-const CreateDrink = ({setUserDrinks,userDrinks}) => {
-    const[drinkName,setDrinkName] = useState();
-    const[imgUrl,setImgUrl] = useState();
-    const[instr,setInstr] = useState();
+const CreateDrink = ({setUserDrinks, userDrinks}) => {
+    const [drinkName, setDrinkName] = useState();
+    const [imgUrl, setImgUrl] = useState();
+    const [instr, setInstr] = useState();
+
+    const initialState = {strDrink: "", strInstructions: "", strDrinkThumb: ""};
+    const [drink, setDrink] = useState(initialState);
+    //setDrink(initialState)
 
 
+    const persistDrink = async (d) => {
+        setUserDrinks([...userDrinks, drink]);
+        const res = await fetch('http://localhost:5001/drinks', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(d)
+        })
+        const data = await res.json()
+    }
 
-    const persistDrink = async (e) => {
-        e.preventDefault()
-        const drink ={
-            strDrink:  drinkName,
-            strInstructions: instr,
-            strDrinkThumb: imgUrl
-        }
-        setUserDrinks([...userDrinks,drink]);
-            const res = await fetch('http://localhost:5001/drinks', {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(drink)
-            })
-            const data = await res.json()
-        }
+    const handleInput = (event) => {
+        const target = event.target
+        const id = target.id
+        const value = target.value
+        setDrink({...drink, [id]: value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        persistDrink(drink);
+        setDrink(initialState);
+    }
 
     return (
         <>
-            <form>
+            <form onChange={handleInput} onSubmit={handleSubmit}>
                 <h3>Create drink:</h3>
                 <label>Drink name:</label>
-                <input type={"text"} onChange={(e) => setDrinkName(e.target.value)}/><br/><br/>
+                <input type={"text"} id="strDrink" value={drink.strDrink}/><br/><br/>
                 <label>Image url:</label>
-                <input type={"text"} onChange={(e) => setImgUrl(e.target.value)}/><br/><br/>
+                <input type={"text"} id="strDrinkThumb" value={drink.strDrinkThumb}/><br/><br/>
                 <label>Instruction:</label> <br/>
-                <textarea rows="4" cols="50" onChange={(e) => setInstr(e.target.value)}/><br/>
-                <button type={"submit"} onClick={persistDrink}>Create</button>
+                <textarea rows="4" cols="50" id="strInstructions" value={drink.strInstructions}/><br/>
+                <button type={"submit"}>Create</button>
             </form>
         </>
     );
