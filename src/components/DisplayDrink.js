d1d014d7ff23372a1c829447824d7836a7c2a99b
viggo23111 <78900579+viggo23111@users.noreply.github.com>
2022-03-31 12:43:31 +0200

    small style fixes

--- src/components/DisplayDrink.js
+++ src/components/DisplayDrink.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types'
-import {FaTimes,FaMarker} from 'react-icons/fa'
+import {FaTimes, FaMarker} from 'react-icons/fa'
 
 
-const DisplayDrink = ({drink,id,strDrink,imgUrl,instr,onDelete,setDrinkToUpdate,setUpdatedDrink}) => {
+const DisplayDrink = ({drink, id, strDrink, imgUrl, instr, onDelete, setDrinkToUpdate, setUpdatedDrink}) => {
     return (
         <div className="card-container">
 
-            <div style={id ? {display:"block"} : {display:"none"}}>
-            <p  style={{margin:0,textAlign:"right"}} ><FaTimes style={{color: 'red', cursor: 'pointer'}} onClick={() => onDelete(id)}/></p>
-
-                <p><FaMarker style={{cursor: 'pointer'}} onClick={() => {setDrinkToUpdate(drink)
-                setUpdatedDrink(drink)
-                }}/></p>
+            <div className="row" style={id ? {display: "block"} : {display: "none"}}>
+                <div className="column">
+                    <p style={{margin: 0, textAlign:"left"}}><FaMarker style={{cursor: 'pointer'}} onClick={() => {
+                        setDrinkToUpdate(drink)
+                        setUpdatedDrink(drink)
+                    }}/></p>
+                </div>
+                <div className="column">
+                    <p style={{margin: 0, textAlign: "right"}}><FaTimes style={{color: 'red', cursor: 'pointer'}}
+                                                                        onClick={() => onDelete(id)}/></p>
+                </div>
             </div>
             <h3>{strDrink}</h3>
             <img alt={"No img found"} src={imgUrl} width={200} height={200}/>
             <h4>Instructions:</h4>
             <p>{instr}</p>
         </div>
+
+
     );
 };
-
-
 
 
 DisplayDrink.propTypes = {
